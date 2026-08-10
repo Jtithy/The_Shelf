88b67a008257c8161037fafda3a14d57f6f76ec0
jtithy <tithyjebunnesa202@gmail.com>
2026-08-10 22:10:28 +0600

    GET api books

--- Backend/server.js
+++ Backend/server.js
@@ -18,6 +18,31 @@ app.get("/", (req, res) => {
     res.send("The_Shelf backend is running!");
 });
 
+//Get All Books
+app.get("/api?books", (req, res) => {
+    const sql = `
+    SELECT
+        id,
+        title,
+        author,
+        cover,
+        rating,
+        review,
+        dateAdded
+    FROM books
+    ORDER BY dateADDED DESC`;
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error("Error retrieving books: ", err);
+            return res.status(500).json({
+                message: "Failed to retrieve books from the database."
+            });
+        }
+        res.json(results);
+    });
+});
+
 //Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
