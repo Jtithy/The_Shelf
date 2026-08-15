1fd96c371d046a035c1e003c94edb77eebf6b57a
jtithy <tithyjebunnesa202@gmail.com>
2026-08-15 07:58:15 +0600

    Electron app set up

---
+++ Electron/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require("path");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreference: {
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+
+    //Load index.html
+
+    win.loadFile(path.join(__dirname, "..", "Frontend", "index.html"));
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
